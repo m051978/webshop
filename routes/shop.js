499e19484fc1d75835eb43fa9468aba44be5377b
m051978 <holland051978@gmail.com>
2023-01-22 13:06:37 +0100

    Initial commit

---
+++ routes/shop.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+// To display cart
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
